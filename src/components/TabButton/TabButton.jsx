8504202b6f78bf3f2fe8e81f29893b77cfe37e15
Vladyslav ALISOV <vladyslav.alisov@gmail.com>
2024-08-05 17:11:16 +0300

    first commit

---
+++ src/components/TabButton/TabButton.jsx
@@ -0,0 +1,3 @@
+export default function TabButton({children, onSelect, isSelected}) {
+    return <li><button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button></li>
+}
